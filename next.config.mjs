5cba64d74aae8e5c3959d328b78e5e54306ebbf0
Ishita-2-dev <ishitajassal210official@gmail.com>
2025-05-03 16:48:49 +0530

    update dependencies and lock files

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,7 @@ const nextConfig = {
                 hostname: 'res.cloudinary.com',
                 pathname: '**',
             },
-            {
-                eslint: {
-                    ignoreDuringBuilds: true
-                }
-            },
+           
             {
                 protocol: 'https',
                 hostname: 'raw.githubusercontent.com',
